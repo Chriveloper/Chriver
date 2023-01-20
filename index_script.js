a88d136bbd9a3d11f307fab3ef149c6232644346
Christian Liu <christianliumunich@gmail.com>
2023-01-20 12:42:57 +0100

    hei

---
+++ index_script.js
@@ -0,0 +1,12 @@
+function draw() {
+  const canvas = document.getElementById("canvas");
+  if (canvas.getContext) {
+    const ctx = canvas.getContext("2d");
+
+    ctx.fillStyle = "rgb(200, 0, 0)";
+    ctx.fillRect(10, 10, 50, 50);
+
+    ctx.fillStyle = "rgb(0, 200, 0)";
+    ctx.fillRect(30, 60, 50, 50);
+  }
+}
